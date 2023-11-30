7cda87477d92a020e52731d564b6e8de32b5758f
ProfessorAguiar <113594063+ProfessorAguiar@users.noreply.github.com>
2023-11-30 19:00:44 -0300

    Qtds Atualizadas

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,50 +1,56 @@
 import { Component } from '@angular/core';
 import { Storage, ref, uploadBytes, listAll, getDownloadURL } from '@angular/fire/storage';
 import { doc, collection, setDoc, Firestore, collectionData, getDocs } from '@angular/fire/firestore';
-import { NOMEM } from 'dns';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
-  promos:any=[]
-  promosName:any=[]
-  promosValor:any=[]
-  produto:any=[]
-  valorTotal:any=0
-  count:number = 0;
+  promos: any = []
+  promosName: any = []
+  promosValor: any = []
+  produto: any = []
+  valorTotal: string = '0,00'
+  count: number = 0;
   isModalOpen = false;
-  setOpen(isOpen: boolean, imagem:any, Nome:any, Descricao:any, pValor:any, pQtd:any) {
+  setOpen(isOpen: boolean, imagem: any, Nome: any, Descricao: any, pValor: any, pQtd: any) {
     this.isModalOpen = isOpen;
-    this.produto[0]={pNome:Nome, pDescricao:Descricao, pValor:pValor, pImg:imagem, pQtd:pQtd}
+    this.produto[0] = { pNome: Nome, pDescricao: Descricao, pValor: pValor, pImg: imagem, pQtd: pQtd }
     // this.produto[0]=[...this.produto, {pNome:Nome, pDescricao:Descricao, pValor:pValor, pImg:imagem, pQtd:pQtd}]
   }
   ngOnInit() {
     this.listar()
   }
-  async listar(){
-  const querySnapshot = await getDocs(collection(this.firestore, "promos"));
+  async listar() {
+    const querySnapshot = await getDocs(collection(this.firestore, "promos"));
     querySnapshot.forEach((doc) => {
       console.log(`${doc.id} => ${doc.data()['promocao']}`);
-      this.promos=[...this.promos,{img:doc.data()['img'], produto:doc.data()['promocao'], valor:doc.data()['valor'], descricao:doc.data()['descricao'],qtd:doc.data()['qtd']}]
+      this.promos = [...this.promos, { img: doc.data()['img'], produto: doc.data()['promocao'], valor: doc.data()['valor'], descricao: doc.data()['descricao'], qtd: doc.data()['qtd'] }]
     });
   }
   increment() {
+    if (this.count < this.produto[0].pQtd) {
     this.count++;
-    const a=(this.produto[0].pValor).replace(",", ".")
-    console.log(a*this.count)
-    this.valorTotal=a*this.count
-    
+    const a = (this.produto[0].pValor).replace(",", ".")
+    console.log(a * this.count)
+    this.valorTotal = String((a * this.count).toFixed(2))
+    this.valorTotal = this.valorTotal.replace('.', ',')
+    }
   }
 
   decrement() {
-    this.count--;
-    this.valorTotal=this.produto.pValor*this.count
+    if (this.count > 0) {
+      this.count--;
+      const a = (this.produto[0].pValor).replace(",", ".")
+      console.log(a * this.count)
+      this.valorTotal = String((a * this.count).toFixed(2))
+      this.valorTotal = this.valorTotal.replace('.', ',')
+    }
   }
-  addCarrinho(){
+  addCarrinho() {
     localStorage.setItem('produto', this.produto[0]);
     localStorage.setItem('valor total', this.valorTotal);
   }
-  constructor(private af:Storage, private firestore: Firestore) {}
+  constructor(private af: Storage, private firestore: Firestore) { }
 }
